2bbf69c0c82d4ea2ebc2a2c9a3ea24f5b16663ef
Benjamin Knobloch <bknobs4@icloud.com>
2024-11-19 13:29:20 -0500

    Fixes to existing construct_word algorithm

--- helpers_gameLogic.js
+++ helpers_gameLogic.js
@@ -22,12 +22,13 @@ const construct_word = (query_word, open_tiles, existing_words) => {
   let query_word_letters = query_word.split("");
 
   // Recursive function to find words combination
-  const recur_find_words = (remainingLetters, words, selectedWords = []) => {
+  const recur_find_words = (remainingLetters, words, selectedWords) => {
     // Base case: if no letters remain, a solution is found, return selected words
     if (remainingLetters.length === 0) return selectedWords;
 
     // Try each word in words to see if it helps match remaining letters
     for (let i = 0; i < words.length; i++) {
+      let wordWorks = true;
       let word = words[i];
       let tempRemainingLetters = remainingLetters.slice();
 
@@ -36,8 +37,11 @@ const construct_word = (query_word, open_tiles, existing_words) => {
         let index = tempRemainingLetters.indexOf(letter);
         if (index !== -1) {
           tempRemainingLetters.splice(index, 1);
-        }
+        } else wordWorks = false;
       }
+
+      // If this word doesn't work with the remaining letters, move to the next word.
+      if (!wordWorks) continue;
 
       // Check if the remaining letters were covered by including this word
       if (tempRemainingLetters.length < remainingLetters.length) {
@@ -49,17 +53,18 @@ const construct_word = (query_word, open_tiles, existing_words) => {
           selectedWords.concat(word)
         );
 
+        // If a combination is found, return the words that make it up.
         if (result) {
           return result;
         }
       }
     }
 
-    // If no combination could match, return null
+    // If this combination is a dead end, return null
     return null;
   };
 
-  let result = recur_find_words(query_word_letters, existing_words);
+  let result = recur_find_words(query_word_letters, eligible_words, []);
 
   // Check if the word can be made with center tiles and by stealing an existing word.
 
