3dff6c4361acf8350edb4a0087eb74b65ea567b8
mamorales5523 <90879620+mamorales5523@users.noreply.github.com>
2024-12-05 15:14:29 -0500

    displays error message if mongo connection fails

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -5,19 +5,23 @@ let _connection = undefined;
 let _db = undefined;
 
 export const dbConnection = async () => {
-  if (!_connection) {
-    const client = new MongoClient(mongoConfig.serverUrl, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-    _connection = await client.connect();
-    _db = _connection.db(mongoConfig.database);
-  }
+  try {
+    if (!_connection) {
+      const client = new MongoClient(mongoConfig.serverUrl, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        },
+      });
+      _connection = await client.connect();
+      _db = _connection.db(mongoConfig.database);
+    }
 
-  return _db;
+    return _db;
+  } catch (e) {
+    console.error("Connection failed", e);
+  }
 };
 
 export const closeConnection = async () => {
