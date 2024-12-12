114f9e6444b98b2b387217b13b4e8a3dc5fbb4b1
Benjamin Knobloch <bknobs4@icloud.com>
2024-12-12 12:40:03 -0500

    added game log support and feedback for grabbing (or failing to grab) a word

--- public/js/gameHandler.js
+++ public/js/gameHandler.js
@@ -3,94 +3,98 @@ let table_tiles = document.getElementById("tableTiles");
 let words_in_play = document.getElementById("wordsInPlay");
 let guess_form = document.getElementById("guessForm");
 let current_player = document.getElementById("currentPlayer");
+let updateRibbon = document.getElementById("updateRibbon");
+
 const this_user = document.getElementById("username").value;
 
 draw_button.addEventListener("click", () => {
-  socket.emit("draw", gameId);
+    socket.emit("draw", gameId);
 });
 
 guess_form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  socket.emit("guess", {
-    guess: document.getElementById("guessWord").value,
-    passcode: gameId,
-  });
+    socket.emit("guess", {
+        guess: document.getElementById("guessWord").value,
+        passcode: gameId,
+    });
 
-  document.getElementById("guessWord").value = "";
+    document.getElementById("guessWord").value = "";
 });
 
 socket.on("updateGamestate", (state) => {
-  let gamestate = state.gamestate;
+    let gamestate = state.gamestate;
 
-  if (gamestate.ongoing) {
-    let active_player_name = gamestate.active_player.name;
+    if (gamestate.ongoing) {
+        let active_player_name = gamestate.active_player.name;
 
-    current_player.innerHTML = active_player_name;
+        current_player.innerHTML = active_player_name;
+        updateRibbon.innerHTML =
+            gamestate.gameLog[gamestate.gameLog.length - 1];
 
-    draw_button.disabled = !(this_user === gamestate.active_player.name);
+        draw_button.disabled = !(this_user === gamestate.active_player.name);
 
-    let tiles = [];
-    for (const tile of gamestate.table_tiles) {
-      tiles.push(`
+        let tiles = [];
+        for (const tile of gamestate.table_tiles) {
+            tiles.push(`
             <p class="tile">${tile.toUpperCase()}</p>
             `);
-    }
-
-    table_tiles.innerHTML = tiles.join("");
-
-    let player_words_str = "";
-
-    const colors = [
-      "lightblue",
-      "lightgreen",
-      "lightsalmon",
-      "lavender",
-      "navajowhite",
-      "mediumaquamarine",
-      "tomato",
-    ];
-
-    let i = 0;
-    for (let player in gamestate.players) {
-      let word_list = [];
-      for (const word of gamestate.players[player].words) {
-        let this_word = [];
-        for (const letter of word) {
-          this_word.push(
-            `<p class="tile" style="background-color: ${
-              colors[i]
-            }">${letter.toUpperCase()}</p>`
-          );
         }
-        word_list.push(`
+
+        table_tiles.innerHTML = tiles.join("");
+
+        let player_words_str = "";
+
+        const colors = [
+            "lightblue",
+            "lightgreen",
+            "lightsalmon",
+            "lavender",
+            "navajowhite",
+            "mediumaquamarine",
+            "tomato",
+        ];
+
+        let i = 0;
+        for (let player in gamestate.players) {
+            let word_list = [];
+            for (const word of gamestate.players[player].words) {
+                let this_word = [];
+                for (const letter of word) {
+                    this_word.push(
+                        `<p class="tile" style="background-color: ${
+                            colors[i]
+                        }">${letter.toUpperCase()}</p>`
+                    );
+                }
+                word_list.push(`
                 <div class="word">${this_word.join("")}</div>
             `);
-      }
-      player_words_str += `<span class="username_header">${
-        gamestate.players[player].name
-      }</span>:<br>
+            }
+            player_words_str += `<span class="username_header">${
+                gamestate.players[player].name
+            }</span>:<br>
         ${word_list.join("")}
         <br><br>`;
-      i++;
-    }
+            i++;
+        }
 
-    words_in_play.innerHTML = player_words_str;
-  } else {
-    document.getElementById("gameDiv").innerHTML = `
+        words_in_play.innerHTML = player_words_str;
+    } else {
+        document.getElementById("gameDiv").innerHTML = `
         <h4>GAME CONCLUDED!</h4>
         <h5>The winner is: <span>${
-          gamestate.players.reduce((max, current) => {
-            return current.score() > max.score() ? current : max;
-          }, gamestate.players[0]).name
+            gamestate.players.reduce((max, current) => {
+                return current.score() > max.score() ? current : max;
+            }, gamestate.players[0]).name
         }</span></h5>
         `;
-  }
+    }
 });
 
 // displays socket error
 socket.on("error", (message) => {
-  $("#gameDiv").attr("hidden", true);
-  $("#roomFormError").html(`Error: ${message}`);
-  $("#roomFormError").attr("hidden", false);
+    $("#gameDiv").attr("hidden", true);
+    $("#roomFormError").html(`Error: ${message}`);
+    $("#roomFormError").attr("hidden", false);
 });
