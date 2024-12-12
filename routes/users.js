a230015bf2cfec3a76d57bffdd1e36e54fc252b3
Omar Elkhafif <134241854+oelkhafi@users.noreply.github.com>
2024-12-12 18:45:29 -0500

    added some authentication and fixed some bugs

--- routes/users.js
+++ routes/users.js
@@ -109,6 +109,7 @@ router
       }
 
       const { usernameInput, passwordInput, birthdayInput } = req.body;
+      //console.log("Birthday input before calling on data functions: " + birthdayInput);
 
       let createdUser = await usersData.createUser(
         usernameInput,
@@ -131,6 +132,18 @@ router
   .route("/:userId")
   .get(async (req, res) => {
     try {
+      if (!req.session.user) {
+        return res.redirect("/users/login"); // Redirect to login if not authenticated
+      }
+      // Restrict access to the user who owns the profile
+      if (req.session.user.id !== req.params.userId) {
+        return res.status(403).render("authentication/error", {
+          user: req.session.user,
+          error: "You are not authorized to view this profile.",
+        });
+      }
+
+      
       let user = await usersData.getUserById(req.params.userId);
       delete user.password;
 
